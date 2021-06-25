ba012de72f03c24f7f34d3a4c8b6ffc8ab99fce5
Phillip Luther <dev@phillipluther.com>
2021-06-25 07:45:34 -0700

    Add React SVG plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,14 @@ module.exports = {
     //   },
     // },
     {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /\.inline\.svg/,
+        },
+      },
+    },
+    {
       resolve: 'gatsby-plugin-feed',
       options: {
         query: `
